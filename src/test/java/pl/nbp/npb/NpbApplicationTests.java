acf2598e4e938bdab7bacf2e8ef17428d9d482ef
Dawidek97 <dawid.rykowski@go2.pl>
2019-03-11 12:58:20 +0100

    first

---
+++ src/test/java/pl/nbp/npb/NpbApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.nbp.npb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NpbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
